29acf1913e9004277fc34fe58d51d794edffcd92
Wanyu Jiang <wanyu19930930@gmail.com>
2023-06-22 23:54:39 +0900

    input onChangeHandler

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -8,13 +8,29 @@ const AddUser = (props) => {
     console.log("submit");
   };
 
+  const inputChangeHandler = (input, value) => {
+    console.log(input, value);
+  };
+
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <lable htmlFor="username">Username</lable>
-        <input id="username" type="text" />
+        <input
+          id="username"
+          type="text"
+          onChange={(event) => {
+            inputChangeHandler("username", event.target.value);
+          }}
+        />
         <lable htmlFor="age">Age (Years)</lable>
-        <input id="age" type="number" />
+        <input
+          id="age"
+          type="number"
+          onChange={(event) => {
+            inputChangeHandler("age", event.target.value);
+          }}
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
