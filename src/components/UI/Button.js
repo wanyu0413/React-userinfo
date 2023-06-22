9f7097d54550bbbdaad97b35769a11df199e105f
Wanyu Jiang <wanyu19930930@gmail.com>
2023-06-22 23:08:18 +0900

    Button component

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,8 +1,14 @@
-import classes from './Button.module.css';
+import classes from "./Button.module.css";
 
 const Button = (props) => {
   return (
-
+    <button
+      className={classes.button}
+      type={props.type || "button"}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
   );
 };
 
