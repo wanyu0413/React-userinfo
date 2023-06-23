04e0d8140a2c3a2126dd1d920dd5faaf9cc4bebb
Wanyu Jiang <wanyu19930930@gmail.com>
2023-06-23 14:12:31 +0900

    add UserList to the App

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,6 +1,17 @@
+import Card from "../UI/Card";
+import classes from "./UsersList.module.css";
+
 const UsersList = (props) => {
   return (
-
+    <Card className={classes.users}>
+      <ul>
+        {props.users.map((user) => (
+          <li>
+            {user.name} ({user.age} years old)
+          </li>
+        ))}
+      </ul>
+    </Card>
   );
 };
 
