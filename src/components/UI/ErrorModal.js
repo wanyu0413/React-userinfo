257503be12a3c8604ed931195039cf0ace44d9de
Wanyu Jiang <wanyu19930930@gmail.com>
2023-06-24 13:21:39 +0900

    import Button component to ErrorModal.js

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,7 +1,20 @@
 import Card from "./Card";
+import Button from "./Button";
 
 const ErrorModal = (props) => {
-  return <Card></Card>;
+  return (
+    <Card>
+      <header>
+        <h2></h2>
+      </header>
+      <div>
+        <p></p>
+      </div>
+      <footer>
+        <Button></Button>
+      </footer>
+    </Card>
+  );
 };
 
 export default ErrorModal;
