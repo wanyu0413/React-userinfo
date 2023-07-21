29b5807f5a7eac174cbb7ae43b315efd1ee72a4c
Wanyu Jiang <wanyu19930930@gmail.com>
2023-07-21 19:05:17 +0900

    React.Fragment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
 
@@ -15,10 +15,12 @@ function App() {
   };
 
   return (
-    <div>
+    // <React.Fragment>
+    <>
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
-    </div>
+    </>
+    // </React.Fragment>
   );
 }
 
