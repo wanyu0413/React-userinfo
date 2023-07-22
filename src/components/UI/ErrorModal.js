0b6e5445ee73757c65fc80cd51e428b9f57b6f1d
Wanyu Jiang <wanyu19930930@gmail.com>
2023-07-23 01:29:01 +0900

    modaloverlay creatPortal

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -32,6 +32,14 @@ const ErrorModal = (props) => {
         <Backdrop onConfirm={props.onConfirm} />,
         document.getElementById("backdrop-root")
       )}
+      {ReactDOM.createPortal(
+        <Modaloverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById("overlay-root")
+      )}
     </React.Fragment>
   );
 };
